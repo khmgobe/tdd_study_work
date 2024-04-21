6b33b2013d37163fd1292af7b44e2f5583a93806
min <dr94406@naver.com>
2024-04-21 18:52:08 +0900

    🔴red-test: 사용자 비밀번호 해시 처리 테스트 작성

---
+++ src/test/java/sample/tdd_study_work/board/admin/service/AdminServiceTest.java
@@ -0,0 +1,42 @@
+package sample.tdd_study_work.board.admin.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AdminServiceTest {
+
+    private final AdminService adminService = new AdminService();
+
+    @DisplayName("사용자의 비밀번호가 변환되었는지 확인합니다.")
+    @Test
+    void hashPassword() {
+
+    //given
+    String password = "";
+
+    //when
+    String hashedPassword = "";
+
+
+    //then
+    assertThat(password).isNotEqualTo(hashedPassword);
+
+    }
+
+    @DisplayName("사용자의 비밀번호와 해시처리된 비밀번호를 비교하여 확인합니다.")
+    @Test
+    void checkPassword() {
+
+        //given
+        String password = "khm123";
+        String hashedPassword = "khm123";
+
+        //when
+        boolean passwordMatches = adminService.checkPassword(password, hashedPassword);
+
+        //then
+        assertThat(passwordMatches).isTrue();
+    }
+}
