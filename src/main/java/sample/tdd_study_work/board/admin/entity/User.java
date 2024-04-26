adf181eeccc2086a59048154e863238de686542f
min <dr94406@naver.com>
2024-04-26 22:03:53 +0900

    ⭐feat: 유저 엔티티 데이터 세팅

--- src/main/java/sample/tdd_study_work/board/admin/entity/User.java
+++ src/main/java/sample/tdd_study_work/board/admin/entity/User.java
@@ -1,11 +1,12 @@
 package sample.tdd_study_work.board.admin.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
+import sample.tdd_study_work.board.admin.dto.UserCreate;
+import sample.tdd_study_work.board.admin.dto.UserRead;
+import sample.tdd_study_work.board.admin.enumeration.UserAuthorityStatus;
 import java.time.LocalDate;
 
 @Entity
@@ -23,5 +24,49 @@ public class User {
     private LocalDate birthDay;
 
     private String email;
-}
 
+    @Enumerated(EnumType.STRING)
+    private UserAuthorityStatus userAuthorityStatus;
+
+    @Builder
+    private User(Long id, String name, String password, LocalDate birthDay, String email, UserAuthorityStatus userAuthorityStatus) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.birthDay = birthDay;
+        this.email = email;
+        this.userAuthorityStatus = userAuthorityStatus;
+    }
+
+    public UserCreate of() {
+        return UserCreate.builder().
+                name(name).password(password).
+                birthDay(birthDay).
+                email(email).
+                userAuthorityStatus(userAuthorityStatus).
+                build();
+    }
+
+    public UserRead of2() {
+        return UserRead.builder().
+                name(name).password(password).
+                birthDay(birthDay).
+                email(email).
+                userAuthorityStatus(userAuthorityStatus).
+                build();
+    }
+
+    public boolean isAdmin() {
+        if(userAuthorityStatus.equals(UserAuthorityStatus.ADMIN)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isValid() {
+        if(email != null && password != null) {
+            return true;
+        }
+        return false;
+    }
+}
