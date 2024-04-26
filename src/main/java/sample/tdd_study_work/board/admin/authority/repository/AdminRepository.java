bced1fe06a2321b50b4811884848f3e169b14412
min <dr94406@naver.com>
2024-04-26 23:45:48 +0900

    🐞fix: 관리자 레포지토리 인터페이스 함수 추가

--- src/main/java/sample/tdd_study_work/board/admin/authority/repository/AdminRepository.java
+++ src/main/java/sample/tdd_study_work/board/admin/authority/repository/AdminRepository.java
@@ -1,9 +1,13 @@
 package sample.tdd_study_work.board.admin.authority.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import sample.tdd_study_work.board.admin.entity.User;
 
-@Repository
 public interface AdminRepository extends JpaRepository<User, Long> {
+
+    User findByEmailAndPassword(String email, String password);
+
+    boolean existsByEmail(String email);
+
+    User findByEmail(String email);
 }
