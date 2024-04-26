2e26744ff661ea13f2467976bed3166b875b4d11
min <dr94406@naver.com>
2024-04-26 23:42:58 +0900

    🐞fix: 유저 삭제 디티오 변경

--- src/main/java/sample/tdd_study_work/board/admin/dto/UserDelete.java
+++ src/main/java/sample/tdd_study_work/board/admin/dto/UserDelete.java
@@ -2,5 +2,5 @@ package sample.tdd_study_work.board.admin.dto;
 
 import java.time.LocalDate;
 
-public record UserDelete (String name, String password, LocalDate birthDay, String email) {
+public record UserDelete (Long id, String name, String password, LocalDate birthDay, String email) {
 }
