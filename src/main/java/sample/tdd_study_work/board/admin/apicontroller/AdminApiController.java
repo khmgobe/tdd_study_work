22a75cfabec7d5998bb7a2f80fa2882c2c0809d2
min <dr94406@naver.com>
2024-04-26 23:45:32 +0900

    🐞fix: 관리자 에이피아이 수정

--- src/main/java/sample/tdd_study_work/board/admin/apicontroller/AdminApiController.java
+++ src/main/java/sample/tdd_study_work/board/admin/apicontroller/AdminApiController.java
@@ -1,14 +1,11 @@
 package sample.tdd_study_work.board.admin.apicontroller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
-import sample.tdd_study_work.board.admin.dto.UserCreate;
-import sample.tdd_study_work.board.admin.dto.UserDelete;
-import sample.tdd_study_work.board.admin.dto.UserUpdate;
+import org.springframework.web.bind.annotation.*;
+import sample.tdd_study_work.board.admin.authority.enumeration.BoardAuthorityStatus;
+import sample.tdd_study_work.board.admin.dto.*;
 import sample.tdd_study_work.board.admin.service.AdminService;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,23 +13,39 @@ public class AdminApiController {
 
     private final AdminService adminService;
 
-    @PostMapping("/api/v1/admin/hash")
-    public String hashPassword(String password) {
-        return adminService.hashPassword(password);
+    @PostMapping("/api/v1/admin/login")
+    public void login(@RequestBody UserLogin userLogin) {
+         adminService.login(userLogin);
     }
+
 
     @PostMapping("/api/v1/admin")
-    public void createUser(UserCreate userCreate) {
-
+    public Long create(@RequestBody UserCreate userCreate) {
+        return adminService.create(userCreate);
     }
 
-    @DeleteMapping("/api/v1/admin")
-    public void deleteUser(UserDelete userDelete) {
+    @DeleteMapping("/api/v1/admin/{id}")
+    public void delete(@PathVariable Long id) {
+        adminService.delete(id);
 
     }
+    @PatchMapping("/api/v1/admin")
+    public void update(@RequestBody UserUpdate userUpdate) {
+        adminService.update(userUpdate);
+    }
 
-    @PutMapping("/api/v1/admin")
-    public void updateUser(UserUpdate userUpdate) {
+    @GetMapping("/api/v1/admin")
+    public List<UserRead> findAll() {
+        return adminService.findAll();
+    }
 
+    @PatchMapping("/api/v1/admin/reset/{id}")
+    public void reset(@PathVariable Long id) {
+        adminService.reset(id);
+    }
+
+    @PatchMapping("/api/v1/admin/permission/{id}")
+    public void updatePermission(@PathVariable Long id, @RequestBody BoardAuthorityStatus boardAuthorityStatus) {
+        adminService.updatePermission(id, boardAuthorityStatus);
     }
 }
